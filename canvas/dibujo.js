cc3ce681b67021c63a8d7458910c61312f9ef3e4
BoroYagami <36431716+BoroYagami@users.noreply.github.com>
2018-02-16 10:51:22 -0700

    Ciclo For con canvas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,8 +1,20 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas =30;
+var l = 0;
+var yi, xf;
+var colorcito = "#FAA";
 
-DibujarLinea("pink", 10, 300, 220, 10);
-DibujarLinea("yelow", 300, 10, 10, 220);
+for (l = 0; l < lineas; l++)
+{
+  yi = 10 * l;
+  xf = 10 * l;
+  DibujarLinea(colorcito, 0, yi, xf, 300);
+  console.log("Linea " + 1);
+}
+
+DibujarLinea(colorcito, 1, 1, 1, 299);
+DibujarLinea(colorcito, 1, 299, 299, 299);
 
 function DibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
