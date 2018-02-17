bb1cb984092b44ff56bdaf77297e4599f1fb61a0
BoroYagami <xllsdllxlalo@gmail.com>
2018-02-16 20:54:20 -0700

    Esta canvas concluido

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -5,11 +5,25 @@ var l = 0;
 var yi, xf, xi;
 var colorcito = "#FAA";
 var yf = 150;
+var yf2 = 160;
+var yf3 = 150;
+var xf1 = 150;
+var xf2 = 300
+var yi1 = 150;
 
 for (l = 0; l < lineas; l++)
 {
   xi = 10 * l;
   yf = yf - 10;
+  yi = 10 * l;
+  xf1 = xf1 + 10;
+  xi = 10 * l;
+  yf3 = yf3 + 10;
+  yi1 = yi1 + 10;
+  xf2 = xf2 - 10;
+  DibujarLinea(colorcito, 150, yi1, xf2, 150);
+  DibujarLinea(colorcito, xi, 150, 150, yf3);
+  DibujarLinea(colorcito, 150, yi, xf1, 150);
   DibujarLinea(colorcito, xi, 150, 150, yf);
   console.log("Linea " + 1);
 }
@@ -18,7 +32,6 @@ DibujarLinea(colorcito, 1, 1, 1, 299);
 DibujarLinea(colorcito, 1, 299, 299, 299);
 DibujarLinea(colorcito, 299, 299, 299, 1);
 DibujarLinea(colorcito, 299, 1, 1, 1);
-
 DibujarLinea(colorcito, 150, 1, 150, 299);
 DibujarLinea(colorcito, 1, 150, 299, 150);
 
